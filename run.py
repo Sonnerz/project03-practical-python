4d8d6a061053245f92c568d88aef4a455a9593b9
Sonya Cooley <sonyacooley@gmail.com>
2018-08-01 19:15:24 +0000

    Basic HTML and Check and Add Username

--- run.py
+++ run.py
@@ -1,43 +1,38 @@
 import os
 from riddlesList import * 
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, redirect, url_for
 
 app = Flask(__name__)
 app.secret_key = 'The cat is on the roof'
 usernames = []
 attempts = 0
 
-def checkUsername(username):
-    if username not in usernames:
-        usernames.append(username)
-        print(usernames)
-        flash("Success {}, your name is added. Click play".format(username))
-    else:
-        flash("Fail {}, Name taken try again".format(username))
-    return
+
+
 
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        username = request.form["addusername"]
-        checkUsername(username)
+        username = request.form["addusername"].title()
+    
+        if username not in usernames:
+            usernames.append(username)
+            flash("Success {}, your name is added. Click play".format(username))
+            return redirect(url_for('play'))
+        else:
+            flash("Fail {}, Name taken try again".format(username)) 
     return render_template("index.html", page_title="Riddle-Me-This - Home")
     
 
 
 
 
-
-
-
-
-
-    
 @app.route('/play', methods=["GET", "POST"])
 def play():
     if request.method == "POST":
         print(request.form)
     return render_template("play.html", page_title="Riddle-Me-This - Play")    
+   
 
 
 if __name__ == '__main__':
--- test_riddle.py
+++ test_riddle.py
@@ -1,5 +1,13 @@
 import unittest
 
-class test_riddle(unittest.TestCase):
+class TestRiddle(unittest.TestCase):
     def test_is_this_thing_on(self):
         self.assertEqual(1,1)
+        
+    def test_is_in(self):
+        assert item in collection, "{0} does not contain {1}".format(collection, item)
+        
+
+print(test_is_in(["dog", "cat", "bird"], "fish"))        
+        
+        
