9408329d96f02aabaf32989ea79f0e487bd93bd2
Sonya Cooley <sonyacooley@gmail.com>
2018-08-14 18:04:59 +0000

    Bug fix after Mentor call, Multiple players can play successfully now. I had muddled up passing the Riddle Index

--- run.py
+++ run.py
@@ -28,7 +28,6 @@ def start_game():
     Get the first riddle to start the game. Redirect to the game html page - play() play.html.
     '''
     date = datetime.now().strftime("%d-%m-%Y")
-    flash(date)
     leaderboard.append({"username":session['username'], "score":0, "timestamp":date}) # added to leaderboard
     player_info.append({"username":session['username'], "score":0, "attempt":0, "riddle_number":0}) #creates a player
     global riddle
@@ -39,8 +38,7 @@ def start_game():
 @app.route('/play_v4')
 def play():
     return render_template("play_v4.html", page_title="Riddle-Me-This - Play", username=session['username'], leaderboard=leaderboard, 
-                                    player_info=player_info, usernames=usernames,
-                                    riddle_question=riddle['Question'], riddle_answer=riddle['Answer'], riddle_index=riddle['Number'])                                
+                                    player_info=player_info, usernames=usernames, riddle=riddle)                                
 
 @app.route('/end_v4')
 def end():
@@ -50,7 +48,7 @@ def end():
                                     players=player_info, usernames=usernames, newlist=newlist) 
 
 
-def check_answer(answerInputByPlayer, correct_answer, riddle_index):
+def check_answer(answerInputByPlayer, correct_answer):
     '''
     Based on the username in session the inputed answer is compared against the actual riddle answer.
     If the answer is correct, the score is increased by 1, the leaderboard is updated. The index is increased by one to find next riddle in riddle list []
@@ -69,9 +67,7 @@ def check_answer(answerInputByPlayer, correct_answer, riddle_index):
                     if leader['username'] == session['username']:
                         leader['score'] += 1 # update leaderboard by 1
                 #riddle_index += 1 # increase index by 1 for next riddle
-                if (riddle_index < len(riddles)): # check for last riddle
-                    return player['riddle_number'] 
-                else:
+                if (player['riddle_number'] < len(riddles)): # check for last riddle
                     return player['riddle_number'] 
             else:
                 flash("WRONG try again, one more attempt")
@@ -80,11 +76,9 @@ def check_answer(answerInputByPlayer, correct_answer, riddle_index):
                     player['attempt'] = 0 # reset attempts back to 0
                     player['riddle_number'] += 1
                     #riddle_index += 1 # attempts over, next question
-                    if (riddle_index < len(riddles)): #check for last riddle
+                    if (player['riddle_number'] < len(riddles)): #check for last riddle
                         return player['riddle_number'] 
-                    else:
-                        return player['riddle_number'] 
-            return riddle_index # index of next riddle
+            return player['riddle_number'] # index of next riddle
 
 
 @app.route('/checkPlayerInput', methods=["GET", "POST"])
@@ -101,11 +95,15 @@ def check():
         flash("Players answer: {}".format(player_answer))
         global riddle
         flash("Correct answer: {}".format(riddle["Answer"]))
-        riddle_index = check_answer(player_answer, riddle['Answer'], riddles.index(riddle)) # check_answer() called. Index returned.
-        if riddle_index > 2: # check for last riddle
+        
+        #riddle_number = request.form.get('riddle_number')
+        #print(session['username'], riddle_number)
+        next_riddle_index = check_answer(player_answer, riddle['Answer']) # check_answer() called. Index returned.
+        #print(session['username'], riddle_number)
+        if next_riddle_index > 2: # check for last riddle
             return redirect(url_for('end')) # end of game
         else:
-            riddle = get_next_riddle(riddle_index) # not end of game, get next riddle
+            riddle = get_next_riddle(next_riddle_index) # not end of game, get next riddle
     return redirect(url_for('play'))    
 
 
@@ -114,13 +112,12 @@ def index():
     '''
     Get username from player and check if it's already taken
     '''
-    mess = "woohooo!!!"
     if request.method == "POST":
         username = request.form['addUsername'].title() # username from from input
         if username not in usernames:
             usernames.append(username) # if its a new unique username, add to usernames[]
             session['username'] = username # add username to flask session
-            flash("Success {}, your name is added. Her is your first question".format(username))
+            flash("Success {}, your name is added. Her is your first question. The session is: {}".format(username, session))
             return redirect(url_for('start_game')) # start the game
         else:
             flash("Fail {}, Name taken try again".format(username)) # username taken try again until username is not in usernames[]  
@@ -132,7 +129,7 @@ if __name__ == '__main__':
     
     
     
-    
+    #riddles.index(riddle)
     
     
     
